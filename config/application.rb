a3d0ead3b8a46f7cd6f08b1bb51227cc9bfc6955
YuSa0-6 <fuku.sugar@icloud.com>
2023-12-05 11:43:57 +0900

    [fix] pending migrate error

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module BackDev
   class Application < Rails::Application
-    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
@@ -19,5 +18,10 @@ module BackDev
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
   end
 end
